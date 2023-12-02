0b27d78c9a543655c860266259a4a1c7f4d9cbd2
erliwanderson <wandersonerli.dev@gmail.com>
2023-12-02 03:29:20 -0300

    :iphones: fix: Ajustando responsividade do mobile first

--- myPortfolio/src/components/Hero.tsx
+++ myPortfolio/src/components/Hero.tsx
@@ -2,8 +2,8 @@ import { HiDownload } from "react-icons/hi";
 import img from "../assets/img.jpg";
 function Hero() {
   return (
-    <section className="bg-gradient-to-tr from-black to-gray-700 text-white ">
-      <div className="container mx-auto max-w-4xl p-2 py-6 flex flex-col md:flex-row">
+    <section className="bg-gradient-to-tr pl-3 from-black to-gray-700 text-white ">
+      <div className="container mx-auto max-w-5xl p-2 py-6 flex flex-col md:flex-row">
         <div className="basis-1/2">
           <h1 className="mb-6 text-center md:text-left ">
             <span className="font-handwriting block text-3xl text-center md:text-left">
@@ -16,7 +16,7 @@ function Hero() {
               Erli
             </span>
           </h1>
-          <h2 className="font-semibold flex items-center gap-2 text-center md:text-left">
+          <h2 className="font-semibold flex items-center gap-2 justify-center md:justify-start">
             <div className="h-1 w-12 rounded-full bg-blue-800  "></div>
             Desenvolvedor React.js Front-end
           </h2>
@@ -33,7 +33,7 @@ function Hero() {
             <a
               href="#"
               className=" flex items-center gap-2 button text-gray-500 hover:text-gray-900">
-              <HiDownload  />
+              <HiDownload />
               Baixe meu CV
             </a>
           </div>
