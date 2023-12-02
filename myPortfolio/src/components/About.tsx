38803eba4091cc00f415e58af761d21bb229f780
erliwanderson <wandersonerli.dev@gmail.com>
2023-12-02 03:43:35 -0300

    :sparkles: feat: Criação do SOBRE

--- myPortfolio/src/components/About.tsx
+++ myPortfolio/src/components/About.tsx
@@ -1,8 +1,22 @@
 function About() {
   return (
-    <>
-      <h2 className="text-4xl underline font-bold">About test</h2>
-    </>
+    <section className="container mx-auto my-4 max-w-5xl p-4">
+      <div className="relative p-4 text-center">
+        <h2 className="relative text-blue-900 font-bold z-10">
+          <span className="mr-2 font-headline text-3xl ">Sobre</span>
+          <span className="font-handwriting text-4xl">Mim</span>
+        </h2>
+        <p className="relative text-sm text-gray-600">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, rem
+          eaque voluptate quas illo omnis sapiente laboriosam facilis maxime in
+          vitae accusamus, assumenda necessitatibus ex voluptas iure quae et
+          accusantium, distinctio exercitationem quidem doloremque. Eaque modi
+          laudantium doloribus omnis amet, aperiam temporibus. Placeat, dolores?
+          Distinctio tenetur ullam vero alias quisquam.
+        </p>
+        <div className="absolute left-1/2 top-3 z-0 h-10 rounded-lg bg-blue-100/40"></div>
+      </div>
+    </section>
   );
 }
 
