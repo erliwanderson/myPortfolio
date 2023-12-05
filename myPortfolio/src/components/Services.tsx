029f1c8320bb4027449cc829656d1a95596a90f2
erliwanderson <wandersonerli.dev@gmail.com>
2023-12-05 11:58:26 -0300

    :sparkles: feat: Atualização da paleta de cor

--- myPortfolio/src/components/Services.tsx
+++ myPortfolio/src/components/Services.tsx
@@ -21,8 +21,8 @@ function Services() {
           O que eu faço de melhor
         </p>
         <h2 className="mb-2 font-bold text-blue-900">
-          <span className="mr-2 font-headline text-3xl"></span>
-          <span className="font-handwriting text-4xl"></span>
+          <span className="mr-2 font-headline text-3xl">Meus</span>
+          <span className="font-handwriting text-4xl">Serviços</span>
         </h2>
 
         <p className="text-sm text-gray-600">
@@ -37,7 +37,7 @@ function Services() {
       <div className="mt-6 flex flex-col gap-2 md:flex-row justify-center ">
         {services.map((service, index) => (
           <div
-            className="basis-1/3 rounded-lg bg-gradient-to-tr from-black to-gray-700 p-4 text-white cursor-pointer transition-shadow hover:shadow-2xl"
+            className="basis-1/3 rounded-lg bg-gradient-to-tr from-black to-blue-800 p-4 text-white cursor-pointer transition-shadow hover:shadow-2xl"
             key={index}>
             <div className="mb-2 flex items-center justify-center">
               {service.icon}
